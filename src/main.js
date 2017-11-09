3174c512ef708128043dba921f08226c390a4275
Daniel Savage (Skybox Labs Inc) <v-dasava@microsoft.com>
2017-11-09 12:26:51 -0800

    WIP: Port Pixi.js shaders to ES 3

--- src/main.js
+++ src/main.js
@@ -1,22 +1,29 @@
 let Gameplay = function () {
     this.filterTargetImage = null;
     this.filter = null;
-
-    this.cameraMatrix = mat4.create();
 };
 Gameplay.prototype.preload = function () {
     this.game.load.shader('march', 'shader/hg_sdf.glsl');
 };
 Gameplay.prototype.create = function () {
-    this.filter = new Phaser.Filter(this.game, null, this.game.cache.getShader('march'));
+    let filterUniforms = {
+	camera: { type: 'mat4', value: mat4.create() },
+	sphereTransform: { type: 'mat4', value: mat4.create() },
+	sphereRadius: { type: '1f', value: 64 }
+    };
+    this.filter = new Phaser.Filter(this.game, filterUniforms, this.game.cache.getShader('march'));
 
     this.filterTargetImage = this.game.add.sprite(0, 0);
     this.filterTargetImage.width = this.game.width;
     this.filterTargetImage.height = this.game.height;
     this.filterTargetImage.filters = [ this.filter ];
+
+    mat4.translate(this.filter.uniforms.camera.value, this.filter.uniforms.camera.value, vec3.fromValues(0, 0, -100));
+    this.filter.update();
 };
 Gameplay.prototype.update = function () {
-    //
+    //mat4.rotate(this.filter.uniforms.camera.value, this.filter.uniforms.camera.value, 0.01, vec3.fromValues(1, 0, 0));
+    //this.filter.update();
 };
 
 let main = function () {
